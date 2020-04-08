2ffa41391d5495771a5325ca4cda11f56868813c
derek-cornelison <dcornelison1216@gmail.com>
2020-04-08 11:25:23 -0400

    MVP and stretch goal completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionsScore, setLionsScore] = useState(32);
-  const [tigersScore, setTigersScore] = useState(32);
+  const [lionsScore, setLionsScore] = useState(14);
+  const [tigersScore, setTigersScore] = useState(14);
+  const [quarter, setQuarter] = useState(1);
 
   return (
     <div className="container">
@@ -25,7 +25,24 @@ function App() {
             <div className="away__score">{tigersScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <div className="bottomRow">
+          <div className="down">
+            <h3 className="down__title">Down</h3>
+            <div className="down__value">3</div>
+          </div>
+          <div className="toGo">
+            <h3 className="toGo__title">To Go</h3>
+            <div className="toGo__value">7</div>
+          </div>
+          <div className="ballOn">
+            <h3 className="ballOn__title">Ball on</h3>
+            <div className="ballOn__value">21</div>
+          </div>
+          <div className="quarter">
+            <h3 className="quarter__title">Quarter</h3>
+            <div className="quarter__value">{quarter}</div>
+          </div>
+        </div>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -36,6 +53,9 @@ function App() {
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setTigersScore(tigersScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setTigersScore(tigersScore + 3)}>Away Field Goal</button>
+        </div>
+        <div className="quarterButton">
+          <button className="nextQuarterButton" onClick={() => setQuarter(quarter + 1)}>Next Quarter</button>
         </div>
       </section>
     </div>
